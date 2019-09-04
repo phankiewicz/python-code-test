fb417f9cf25b7aa6a30affb67b8e69d7b5e7ce8d
Piotr Hankiewicz <P.Hankiewicz@gmail.com>
2019-09-04 18:36:30 +0200

    feature: add `starship_class` field filter

--- shiptrader/filters.py
+++ shiptrader/filters.py
@@ -1,6 +1,18 @@
 from django_filters import rest_framework as filters
 
+from .models import Starship
+
 
 class ListingFilter(filters.FilterSet):
     price = filters.RangeFilter()
     created = filters.DateTimeFromToRangeFilter()
+    ship_type__starship_class = filters.ChoiceFilter(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['ship_type__starship_class'].extra['choices'] = [
+            (starship_class, starship_class)
+            for starship_class in Starship.objects.values_list(
+                'starship_class', flat=True
+            ).distinct()
+        ]
