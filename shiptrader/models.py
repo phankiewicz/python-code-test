fc9df492a86d88cc41faa4860d8a78eed3fd828b
Piotr Hankiewicz <P.Hankiewicz@gmail.com>
2019-09-05 15:09:38 +0200

    feature: make numerical field nullable to adjust to swapi schema

--- shiptrader/models.py
+++ shiptrader/models.py
@@ -5,12 +5,12 @@ class Starship(models.Model):
     starship_class = models.CharField(max_length=255)
     manufacturer = models.CharField(max_length=255)
 
-    length = models.FloatField()
-    hyperdrive_rating = models.FloatField()
-    cargo_capacity = models.BigIntegerField()
+    length = models.FloatField(null=True)
+    hyperdrive_rating = models.FloatField(null=True)
+    cargo_capacity = models.BigIntegerField(null=True)
 
-    crew = models.IntegerField()
-    passengers = models.IntegerField()
+    crew = models.IntegerField(null=True)
+    passengers = models.IntegerField(null=True)
 
     def __str__(self):
         return self.starship_class
